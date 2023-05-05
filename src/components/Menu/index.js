54de01eb2ad6c79dcb8a26c8d75e0f064e0a8c94
Connor White <connorwhitepdx@gmail.com>
2023-05-05 16:15:39 -0700

    menu adjustments

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -4,7 +4,7 @@ import './index.css';
 export default function Menu() {
     return (
         <div className='menu'>
-            <div className='logoBox'><img className='logo' src='./images/logo-full-black.svg'></img></div>
+            <div className='logoBox'><img className='logo' src='./images/logo-full-black.svg' alt='sqft logo'></img></div>
             <div className='nav'>
                 <div className='navItem'><a className='navLink'>Buy</a></div>
             </div>
