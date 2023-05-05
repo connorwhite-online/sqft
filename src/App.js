813e7840212733173b148f8784f0d17421ea0f49
Connor White <connorwhitepdx@gmail.com>
2023-05-05 16:01:33 -0700

    css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-// import './App.css';
+import './App.css';
 import Menu from './components/Menu';
 import Home from './pages/Home';
 import Store from './pages/Store';
 
 function App() {
   return (
-    <>
+    <div className='app'>
       <Menu />
       <Home />
       <Store />
-    </>
+    </div>
   );
 }
 
