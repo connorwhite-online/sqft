eb2dd3adf1ba40cd166dbfdd353c35ee1a4adf16
Connor White <connorwhitepdx@gmail.com>
2023-05-05 14:53:12 -0700

    components/pages created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import './App.css';
+// import './App.css';
+import Menu from './components/Menu';
+import Home from './pages/Home';
+import Store from './pages/Store';
 
 function App() {
   return (
     <>
+      <Menu />
+      <Home />
+      <Store />
     </>
   );
 }
---
+++ src/components/Menu/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css';
+
+export default function Menu() {
+    return (
+        <div className='menu'>
+            <div className='logoBox'><img className='logo' src='./images/logo-full-black.svg'></img></div>
+            <div className='nav'>
+                <div className='navItem'><a className='navLink'>Buy</a></div>
+            </div>
+        </div>
+    )
+}
