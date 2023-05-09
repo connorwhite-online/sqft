6fbe714c83de56bf3ff2e33005d1bcdce5a2be1b
Connor White <connorwhitepdx@gmail.com>
2023-05-09 12:18:01 -0700

    full width, pic is 75%

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,7 +4,9 @@ import './index.css';
 export default function Home() {
     return (
         <div className='home'>
-            <img src='./images/P-30.jpeg' alt='home' className='homeImage' />
+            <div className="imageContainer">
+                <img src='./images/P-30.jpeg' alt='home' className='homeImage' />
+            </div>
             <div className='header'>
                 Sq Ft magazine tells stories from the heart and outskirts of the world of art and design. We shine a light on artists and art lovers, and dissect how they shape and are shaped by the spaces they occupy.
             </div>
